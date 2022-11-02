da8beb269b1eb2dce4bffce6b7e586d7000dfaf4
OD <kg-te@naver.com>
2022-11-03 00:53:52 +0900

    :memo: 더미데이터 수정

--- src/constants/attributeDummyData.ts
+++ src/constants/attributeDummyData.ts
@@ -5,10 +5,10 @@ type Segment = {
 };
 
 export const attributeDummyData: Segment[] = [
-	{ value: 'C', name: '소형', type: 'segment' },
-	{ value: 'D', name: '중형', type: 'segment' },
-	{ value: 'E', name: '대형', type: 'segment' },
-	{ value: 'SUV', name: 'SUV', type: 'segment' },
+	{ value: 'C', name: '소형', type: 'attribute' },
+	{ value: 'D', name: '중형', type: 'attribute' },
+	{ value: 'E', name: '대형', type: 'attribute' },
+	{ value: 'SUV', name: 'SUV', type: 'attribute' },
 	{ value: 'gasoline', name: '가솔린', type: 'fuelType' },
 	{ value: 'ev', name: '전기', type: 'fuelType' },
 	{ value: 'hybrid', name: '하이브리드', type: 'fuelType' },
