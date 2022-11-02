0b0740db8e174f30b98c1a29f6f62092d330f2cd
OD <kg-te@naver.com>
2022-11-03 01:07:11 +0900

    :memo: 더미데이터 type 대문자 소문자로 수정

--- src/constants/attributeDummyData.ts
+++ src/constants/attributeDummyData.ts
@@ -5,11 +5,11 @@ type attribute = {
 };
 
 export const attributeDummyData: attribute[] = [
-	{ value: 'ALL', name: '전체', type: 'Segment' },
-	{ value: 'C', name: '소형', type: 'Segment' },
-	{ value: 'D', name: '중형', type: 'Segment' },
-	{ value: 'E', name: '대형', type: 'Segment' },
-	{ value: 'SUV', name: 'SUV', type: 'Segment' },
+	{ value: 'ALL', name: '전체', type: 'segment' },
+	{ value: 'C', name: '소형', type: 'segment' },
+	{ value: 'D', name: '중형', type: 'segment' },
+	{ value: 'E', name: '대형', type: 'segment' },
+	{ value: 'SUV', name: 'SUV', type: 'segment' },
 	{ value: 'gasoline', name: '가솔린', type: 'fuelType' },
 	{ value: 'ev', name: '전기', type: 'fuelType' },
 	{ value: 'hybrid', name: '하이브리드', type: 'fuelType' },
