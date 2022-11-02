5baefaf599f5b400b0ebbf0379a3dda83c820a89
hopak-e <a328028@gmail.com>
2022-11-02 16:45:33 +0900

    💄 Nav 컴포넌트 UI 구현

---
+++ src/components/feature/Nav/index.tsx
@@ -0,0 +1,23 @@
+import { Pagination, Navigation } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import * as S from './styled';
+
+export default function Nav() {
+	return (
+		<>
+			<S.StyledSwiper slidesPerView={5.5} initialSlide={0} spaceBetween={0} loopFillGroupWithBlank={true}>
+				<S.StyledSwiperSlide>1</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>2</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>3</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>4</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>5</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>6</S.StyledSwiperSlide>
+				<S.StyledSwiperSlide>7</S.StyledSwiperSlide>
+			</S.StyledSwiper>
+		</>
+	);
+}
