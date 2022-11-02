a57caaaef28e3b70ff88eab16f378816bdffecd0
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-03 04:54:12 +0900

    :sparkles: 디테일페이지 데이터 관리를 위한 쿼리훅 구현

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import App from './App';
 import { GlobalStyle, theme } from './styles';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 1,
+			useErrorBoundary: true,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<GlobalStyle />
-				<App />
-			</ThemeProvider>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider theme={theme}>
+					<GlobalStyle />
+					<App />
+				</ThemeProvider>
+			</QueryClientProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );
