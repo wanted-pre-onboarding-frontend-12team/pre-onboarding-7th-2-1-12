6288ecf6f1d644b42a3770300b76cbefbb361e77
hopak-e <a328028@gmail.com>
2022-11-02 16:49:05 +0900

    💄 CarList 컴포넌트에 Nav, VehicleItem 컴포넌트 추가

--- src/pages/CarList/index.tsx
+++ src/pages/CarList/index.tsx
@@ -1,10 +1,14 @@
 import * as S from './styled';
 import Button from '../../components/shared/Button';
 import DetailForm from '../../components/feature/DetailForm';
+import Nav from '../../components/feature/Nav';
+import VehicleItem from '../../components/feature/VehicleItem';
 
 const CarList = () => {
 	return (
 		<S.Container>
+			<Nav />
+			<VehicleItem />
 			{false && <S.Message>차량이 없습니다.</S.Message>}
 			{true && <S.Message>불러오는 중</S.Message>}
 		</S.Container>
