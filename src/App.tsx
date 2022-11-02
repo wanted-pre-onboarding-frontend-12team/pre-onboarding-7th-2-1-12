b8b4bd05f429b3e6356cb2fcb1a5914f43275293
OD <kg-te@naver.com>
2022-11-03 04:40:46 +0900

    ✨ react helmet 오픈그래프 메타태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Layout from './components/layout';
 import Router from './router';
+import SEOMetaTag from './components/shared/SEOMetaTag';
 
 const App = () => {
 	return (
 		<Layout>
+			<SEOMetaTag />
 			<Router />
 		</Layout>
 	);
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { GlobalStyle, theme } from './styles';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<GlobalStyle />
-				<App />
-			</ThemeProvider>
+			<HelmetProvider>
+				<ThemeProvider theme={theme}>
+					<GlobalStyle />
+					<App />
+				</ThemeProvider>
+			</HelmetProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );
