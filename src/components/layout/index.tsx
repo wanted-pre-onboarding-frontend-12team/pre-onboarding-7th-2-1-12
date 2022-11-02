bf3cc2add84a0b8e38c2eaf34aaa59e326910ca6
hopak-e <a328028@gmail.com>
2022-11-02 16:48:32 +0900

    💄 layout 컴포넌트에 Header 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,7 +3,12 @@ import * as S from './styled';
 import Header from './Header';
 
 const Layout = (props: PropsWithChildren) => {
-	return <S.Container>{props.children}</S.Container>;
+	return (
+		<S.Container>
+			<Header />
+			{props.children}
+		</S.Container>
+	);
 };
 
 export default Layout;
