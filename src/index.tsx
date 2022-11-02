4a285f20ba1d38c1ec41b178abf6038e3ede86a3
hopak-e <a328028@gmail.com>
2022-11-03 05:07:53 +0900

    🎨 recoil root 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { GlobalStyle, theme } from './styles';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<ThemeProvider theme={theme}>
-				<GlobalStyle />
-				<App />
+				<RecoilRoot>
+					<GlobalStyle />
+					<App />
+				</RecoilRoot>
 			</ThemeProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
