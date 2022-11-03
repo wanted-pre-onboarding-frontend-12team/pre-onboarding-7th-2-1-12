b6ca406188286ba7c6bec4bc947e6c42b147371f
youngmin wi <youngminieo1005@gmail.com>
2022-11-03 15:29:56 +0900

    :lipstick: VehicleItem -> CarListItem 으로 컴포넌트명 변경 & UI 수정 및 props 데이터 바안딩

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,4 +8,4 @@ export { default as Header } from '@src/components/layout/Header';
 // Feature Components
 export { default as DetailForm } from '@src/components/feature/DetailForm';
 export { default as Nav } from '@src/components/feature/Nav';
-export { default as VehicleItem } from '@src/components/feature/VehicleItem';
+export { default as CarListItem } from '@src/components/feature/CarListItem';
